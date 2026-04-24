19aec54cfb657863b6b69da65597eb086bfa2533
cauaresdev <cauarosaesanto@gmail.com>
2026-04-24 08:20:59 -0300

    Commit Final atd3

--- app/src/main/java/com/example/slide3/MainActivity.kt
+++ app/src/main/java/com/example/slide3/MainActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.slide3.databinding.ActivityMainBinding
-import com.example.slide3.R
 
 
 class MainActivity : AppCompatActivity() {
 
-    // O ViewBinding substitui o findViewById, garantindo segurança de nulos e tipos.
     private lateinit var binding: ActivityMainBinding
 
     companion object {
-        // const val: Valores conhecidos em tempo de compilação e imutáveis[cite: 24].
-        const val IDADE_MINIMA_HOMEM = 65 // [cite: 7, 10]
-        const val IDADE_MINIMA_MULHER = 62 // [cite: 7, 11]
+        const val IDADE_MINIMA_HOMEM = 65
+        const val IDADE_MINIMA_MULHER = 62
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calcularAposentadoria() {
-        // val: Definida uma única vez durante a execução[cite: 23].
+
         val idadeInput = binding.etIdade.text.toString()
 
         if (idadeInput.isEmpty()) {
@@ -53,10 +50,9 @@ class MainActivity : AppCompatActivity() {
         val idadeAtual = if (idadeInput.isNotEmpty()) idadeInput.toInt() else 0
         val generoSelecionado = binding.spinnerGenero.selectedItem.toString()
 
-        // var: Valor que precisa ser alterado[cite: 22].
+
         var tempoRestante = 0
 
-        // Lógica de cálculo conforme a especificação
         if (generoSelecionado == "Masculino") {
             tempoRestante = IDADE_MINIMA_HOMEM - idadeAtual
         } else {
@@ -68,10 +64,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun exibirResultado(tempo: Int) {
         if (tempo <= 0) {
-            // Exibe mensagem caso a idade já tenha passado o limite [cite: 13]
+
             binding.tvResultado.text = getString(R.string.ja_aposentado)
         } else {
-            // Estima o tempo restante [cite: 12]
             binding.tvResultado.text = getString(R.string.resultado_formato, tempo)
         }
     }
